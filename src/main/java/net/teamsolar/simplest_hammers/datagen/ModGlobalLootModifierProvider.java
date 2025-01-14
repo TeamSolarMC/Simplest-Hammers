41cc25768354fd3896d26a825d5d37c609fa6410
xeli <sheeteditor2@gmail.com>
2025-01-14 12:57:41 -0500

    datagen: added 15% chance for templates to spawn in village houses

--- src/main/java/net/teamsolar/simplest_hammers/datagen/ModGlobalLootModifierProvider.java
+++ src/main/java/net/teamsolar/simplest_hammers/datagen/ModGlobalLootModifierProvider.java
@@ -1,5 +1,9 @@
 package net.teamsolar.simplest_hammers.datagen;
 
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.LootTable;
 import net.teamsolar.simplest_hammers.SimplestHammers;
 import net.teamsolar.simplest_hammers.loot.ModLootModifier;
 import net.teamsolar.simplest_hammers.item.ModItems;
@@ -12,6 +16,7 @@ import net.neoforged.neoforge.common.data.GlobalLootModifierProvider;
 import net.neoforged.neoforge.common.loot.LootTableIdCondition;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.Pattern;
 
 public class ModGlobalLootModifierProvider extends GlobalLootModifierProvider {
     public ModGlobalLootModifierProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> completableFuture) {
@@ -22,39 +27,67 @@ public class ModGlobalLootModifierProvider extends GlobalLootModifierProvider {
     protected void start() {
         add(
                 "hammer_smithing_template_modifier_in_toolsmith_chests",
-                new ModLootModifier(
-                        new LootItemCondition[]{
-                                LootTableIdCondition.builder(ResourceLocation.withDefaultNamespace("chests/village/village_toolsmith"))
-                                        .and(
-                                                LootItemRandomChanceCondition.randomChance(0.75f)
-                                        ).build()
-                        },
+                toExistingLootPoolWithChance(
+                        "chests/village/village_toolsmith",
+                        0.75f,
                         ModItems.HAMMER_SMITHING_TEMPLATE.get()
                 )
         );
         add(
                 "hammer_smithing_template_modifier_in_armorer_chests",
-                new ModLootModifier(
-                        new LootItemCondition[]{
-                                LootTableIdCondition.builder(ResourceLocation.withDefaultNamespace("chests/village/village_armorer"))
-                                        .and(
-                                                LootItemRandomChanceCondition.randomChance(0.75f)
-                                        ).build()
-                        },
+                toExistingLootPoolWithChance(
+                        "chests/village/village_armorer",
+                        0.75f,
                         ModItems.HAMMER_SMITHING_TEMPLATE.get()
                 )
         );
         add(
                 "hammer_smithing_template_modifier_in_weaponsmith_chests",
-                new ModLootModifier(
-                        new LootItemCondition[]{
-                                LootTableIdCondition.builder(ResourceLocation.withDefaultNamespace("chests/village/village_weaponsmith"))
-                                        .and(
-                                                LootItemRandomChanceCondition.randomChance(0.75f)
-                                        ).build()
-                        },
+                toExistingLootPoolWithChance(
+                        "chests/village/village_weaponsmith",
+                        0.75f,
                         ModItems.HAMMER_SMITHING_TEMPLATE.get()
                 )
         );
+
+        for(ResourceKey<LootTable> table: BuiltInLootTables.all()) {
+            var key = table.location().getPath();
+            // SimplestExcavators.getLogger().info("Datagen checking chest %s".formatted(key));
+            if(key.startsWith("chests/village") && key.matches("chests/village/(.+_house)")) {
+                // SimplestExcavators.getLogger().info("Adding chest loot modifier to %s".formatted(key));
+                add(
+                        "hammer_smithing_template_in_".concat(matchHouseType(key)),
+                        toExistingLootPoolWithChance(
+                                table.location(),
+                                0.15f,
+                                ModItems.HAMMER_SMITHING_TEMPLATE.get()
+                        )
+                );
+            }
+        }
+    }
+
+    private String matchHouseType(String key) {
+        Pattern nameMatcher = Pattern.compile("chests/village/(.+_house)");
+        var results = nameMatcher.matcher(key);
+        results.find();
+        return results.group(1);
+        // SimplestExcavators.getLogger().info("%d".formatted(results.groupCount()));
+        // SimplestExcavators.getLogger().info("%s".formatted(results.matches()));
+    }
+
+    private ModLootModifier toExistingLootPoolWithChance(ResourceLocation location, float chance, Item item) {
+        return new ModLootModifier(
+                new LootItemCondition[]{
+                        LootTableIdCondition.builder(location)
+                                .and(
+                                        LootItemRandomChanceCondition.randomChance(chance)
+                                ).build()
+                },
+                item
+        );
+    }
+    private ModLootModifier toExistingLootPoolWithChance(String location, float chance, Item item) {
+        return toExistingLootPoolWithChance(ResourceLocation.withDefaultNamespace(location), chance, item);
     }
 }
